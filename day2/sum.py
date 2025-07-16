274dd5a77073de845d28fe8013695dce334b3989
luiz Strada <luizfelipestrada@gmail.com>
2025-07-16 16:26:24 -0400

    README additioned day2

--- day2/sum.py
+++ day2/sum.py
@@ -1,5 +1,6 @@
-number1 = input("Type the first number\n")
+number1 = input("Type the firsh number\n")
 number2 = input("Type the second number\n")
 
 sum = int(number1) + int(number2)
-print(f"The sum of numbers is {sum}")
+
+print(f"The sum of the numbers is {sum}")
