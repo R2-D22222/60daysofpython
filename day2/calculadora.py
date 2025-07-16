128555e7bc874aea0d08b7b8dc26873a99c202dc
luiz Strada <luizfelipestrada@gmail.com>
2025-07-16 16:10:08 -0400

    README additioned day

--- day2/calculadora.py
+++
@@ -1,5 +0,0 @@
-number1 = input("Write the firsh number\n")
-number2 = input("Write the second number\n")
-
-sonne = int(number1) + int(number2) 
-print  ( f"This is the sum of numbers: {sonne}")
---
+++ day2/sum.py
@@ -0,0 +1,5 @@
+number1 = input("Type the first number\n")
+number2 = input("Type the second number\n")
+
+sum = int(number1) + int(number2)
+print(f"The sum of numbers is {sum}")
