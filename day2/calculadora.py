edfd04afc9638150e65c13f62d3af72536a0ca94
luiz Strada <luizfelipestrada@gmail.com>
2025-07-16 15:19:18 -0400

    README adicionado dia 2

---
+++ day2/calculadora.py
@@ -0,0 +1,5 @@
+nipplepiercing = input("Write the firsh number\n")
+sweetiefox = input("Write the second number\n")
+
+evaelfie = int(nipplepiercing) + int(sweetiefox) 
+print  ( f"This is the sum of numbers: {evaelfie}")
